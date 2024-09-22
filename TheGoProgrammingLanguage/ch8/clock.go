9beb32c0a91ea8166f5b591a20bd688088fa28f1
Ashish Kumar Verma <ashish.averma12@gmail.com>
2024-09-22 11:50:51 +0530

    channels

---
+++ TheGoProgrammingLanguage/ch8/clock.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept() // Blocks here until a connection is received
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			log.Printf("Client disconnected, %v \n", c.LocalAddr())
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
